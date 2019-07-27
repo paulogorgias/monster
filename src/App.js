88ed74c4688e8a36de48578940de6875a91c55e9
Paulo Górgias <paulo.gorgias@gmail.com>
2019-07-27 02:53:50 -0300

    pre deploy

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from "react";
-import { CardList } from "./components/card-list/card-list.component";
-import "./App.css";
+import React, {Component} from 'react';
+import {CardList} from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component';
+import './App.css';
 
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: '',
     };
   }
 
+  handleChange = e => {
+    this.setState({searchField: e.target.value});
+  };
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/users")
+    fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
-      .then(user => this.setState({ monsters: user }));
+      .then(user => this.setState({monsters: user}));
   }
   render() {
+    const {monsters, searchField} = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()),
+    );
     return (
       <div className="App">
         <h1>Monster Roladex</h1>
-
-        <CardList monsters={this.state.monsters} />
+        <SearchBox
+          placeHolder="Please fill the monster name"
+          handleChange={this.handleChange}
+        />
+        <CardList monsters={filteredMonsters} />
       </div>
     );
   }
