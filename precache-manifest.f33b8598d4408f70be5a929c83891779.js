d65e671eb982e7f32a2d386b65bc60e7b3e69ceb
Paulo Górgias <paulo.gorgias@gmail.com>
2019-07-27 02:53:30 -0300

    Updates

---
+++ precache-manifest.f33b8598d4408f70be5a929c83891779.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b92eb0562c3de4d5bb9aa7be49d353",
+    "url": "/index.html"
+  },
+  {
+    "revision": "882f01df2339bd2fdf13",
+    "url": "/static/css/main.6ee48b8d.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "882f01df2339bd2fdf13",
+    "url": "/static/js/main.8c18b8ba.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
